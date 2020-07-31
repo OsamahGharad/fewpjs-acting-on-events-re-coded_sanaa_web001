7df51a5c4f4b89b39b89721c86d0a83be0292cc7
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-31 20:01:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ document.addEventListener("keydown",function(e){
   {
     let rightNumbers=dodger.style.right.replace("px","");
     let right =parseInt(rightNumbers,10);
-    dodger.style.left= `${right + 1}px`;
+    dodger.style.right= `${right + 1}px`;
 
   }
 })
