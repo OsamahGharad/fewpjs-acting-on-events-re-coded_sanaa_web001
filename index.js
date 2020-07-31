19d26818cbe7e85711b8af35f94c2f70fff8c4a2
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-31 20:05:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ document.addEventListener("keydown",function(e){
   if(e.key === "ArrowLeft"){
     let leftNumbers =dodger.style.left.replace("px","");
     let left=parseInt(leftNumbers,10);
-    if(left<0){
+    if(left>0){
       dodger.style.left= `${left - 1}px`;
     }
 
@@ -33,7 +33,7 @@ document.addEventListener("keydown",function(e){
     let rightNumbers=dodger.style.left.replace("px","");
     let right =parseInt(rightNumbers,10);
 
-    if(right>400){
+    if(right<400){
     dodger.style.left= `${right + 1}px`;
     }
 
